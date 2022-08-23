03ae099245b98fd6a557d2f34379096569e73bd1
crol88 <akcrol@gmail.com>
2022-08-23 14:45:53 +0400

    Update test_login_logout.py

--- test_login_logout.py
+++ test_login_logout.py
@@ -7,39 +7,42 @@ from selenium.webdriver.common.keys import Keys
 
 
 class TestLogin():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
+    def setup_method(self, method):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
 
-  def teardown_method(self, method):
-    self.driver.quit()
+    def test_login(self):
+        # open homepage
+        self.driver.get("https://betav10.ci.dental-pro.online/")
+        self.driver.set_window_size(1920, 1080)
+        # login
+        self.driver.find_element(By.ID, "form_user_loginhtml_input_0_login").send_keys("Директор1")
+        self.driver.find_element(By.ID, "form_user_loginhtml_password_0_password").send_keys("123456")
+        self.driver.find_element(By.ID, "form_user_loginhtml_button_1_button").click()
+        # open cbase
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[1]")))
+        self.driver.find_element(By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[1]").click()
+        self.driver.find_element(By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[2]/a[4]").click()
+        # open form newclient
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='main-container']/div[1]/div/a[4]")))
+        self.driver.find_element(By.XPATH, "//*[@id='main-container']/div[1]/div/a[4]").click()
+        # fill newclient form
+        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-surname']").send_keys("ФамилияАвтоТест")
+        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-name']").send_keys("Имя")
+        self.driver.find_element(By.XPATH,
+                                 "//*[@id='form_cbase_admin_newClient_input_0_wizard[data][second_name]']").send_keys(
+            "Отчество")
+        self.driver.find_element(By.XPATH, "//*[@id='form_cbase_admin_newClient_date_0__visible']").send_keys(
+            "10102010")
+        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-phone']").send_keys("79278889966")
+        self.driver.find_element(By.XPATH, "//*[@id='select2-chosen-9']").click()
+        self.driver.find_element(By.XPATH, "//*[@id='s2id_autogen9_search']").send_keys("2ГИС")
+        self.driver.find_element(By.XPATH, "//*[@id='s2id_autogen9_search']").send_keys(Keys.ENTER)
+        # submit new patient creation
+        self.driver.find_element(By.XPATH, "//*[@id='js-wizard-form']/div[2]/div[1]/button").click()
+        self.driver.find_element(By.XPATH, "//*[@id='js-wizard-form']/div[2]/div[1]/a").click()
 
-  def test_login(self):
-    # Open homepage
-    # self.driver.get("https://stablev10.ci.dental-pro.online/")
-    self.driver.get("https://betav10.ci.dental-pro.online/")
-    self.driver.set_window_size(1920, 1080)
-    # Login
-    self.driver.find_element(By.ID, "form_user_loginhtml_input_0_login").send_keys("Директор1")
-    self.driver.find_element(By.ID, "form_user_loginhtml_password_0_password").send_keys("123456")
-    self.driver.find_element(By.ID, "form_user_loginhtml_button_1_button").click()
-    # Open cbase
-    element = WebDriverWait(self.driver, 10).until(
-      EC.element_to_be_clickable((By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[1]")))
-    self.driver.find_element(By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[1]").click()
-    self.driver.find_element(By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[2]/a[4]").click()
-    # Open form newClient
-    element = WebDriverWait(self.driver, 10).until(
-      EC.element_to_be_clickable((By.XPATH, "//*[@id='main-container']/div[1]/div/a[4]")))
-    self.driver.find_element(By.XPATH, "//*[@id='main-container']/div[1]/div/a[4]").click()
-    # Add new patient
-    self.driver.find_element(By.XPATH, "//*[@id='js-newrec-surname']").send_keys("ФамилияАвтоТест")
-    self.driver.find_element(By.XPATH, "//*[@id='js-newrec-name']").send_keys("Имя")
-    self.driver.find_element(By.XPATH, "//*[@id='form_cbase_admin_newClient_input_0_wizard[data][second_name]']").send_keys("Отчество")
-    self.driver.find_element(By.XPATH, "//*[@id='form_cbase_admin_newClient_date_0__visible']").send_keys("10102010")
-    self.driver.find_element(By.XPATH, "//*[@id='js-newrec-phone']").send_keys("79278889966")
-    self.driver.find_element(By.XPATH, "//*[@id='select2-chosen-9']").click()
-    self.driver.find_element(By.XPATH, "//*[@id='s2id_autogen9_search']").send_keys("2ГИС")
-    self.driver.find_element(By.XPATH, "//*[@id='s2id_autogen9_search']").send_keys(Keys.ENTER)
-    self.driver.find_element(By.XPATH, "//*[@id='js-wizard-form']/div[2]/div[1]/button").click()
-    self.driver.find_element(By.XPATH, "//*[@id='js-wizard-form']/div[2]/div[1]/a").click()
+    def teardown_method(self, method):
+        self.driver.quit()
