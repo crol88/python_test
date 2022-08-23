b968cf242ce239358dab8aa4c92c9d9a359db685
crol88 <akcrol@gmail.com>
2022-08-23 17:56:51 +0400

    настройка параметров

--- test_login_logout.py
+++ test_login_logout.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,41 +8,69 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 
-class TestLogin():
+class TestAddNewPatient(unittest.TestCase):
     def setup_method(self, method):
         self.driver = webdriver.Chrome()
         self.vars = {}
 
-    def test_login(self):
-        # open homepage
+    def test_add_new_patient(self):
+        self.open_homepage()
+        self.login(username="Директор1", password="123456")
+        self.open_cbase()
+        self.open_form_newclient()
+        self.add_key()
+        self.fill_newclient_form(surname="ФамилияАвтоТест", name="Имя", secondname="Отчество",
+                                 datapicker="10102010", phone="79278889966", fromwhere="2ГИС")
+        self.submit_newpatient_creation()
+
+    def test_empty_clientdata(self):
+        self.open_homepage()
+        self.login(username="Директор1", password="123456")
+        self.open_cbase()
+        self.open_form_newclient()
+        self.add_key()
+        self.fill_newclient_form(surname="", name="", secondname="",
+                                 datapicker="", phone="", fromwhere="")
+        self.submit_newpatient_creation()
+
+    def open_homepage(self):
         self.driver.get("https://betav10.ci.dental-pro.online/")
         self.driver.set_window_size(1920, 1080)
-        # login
-        self.driver.find_element(By.ID, "form_user_loginhtml_input_0_login").send_keys("Директор1")
-        self.driver.find_element(By.ID, "form_user_loginhtml_password_0_password").send_keys("123456")
+
+    def login(self, username, password):
+        self.driver.find_element(By.ID, "form_user_loginhtml_input_0_login").send_keys(username)
+        self.driver.find_element(By.ID, "form_user_loginhtml_password_0_password").send_keys(password)
         self.driver.find_element(By.ID, "form_user_loginhtml_button_1_button").click()
-        # open cbase
-        element = WebDriverWait(self.driver, 10).until(
+
+    def open_cbase(self):
+        element = WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[1]")))
         self.driver.find_element(By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[1]").click()
+
+    def open_form_newclient(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[2]/a[4]")))
         self.driver.find_element(By.XPATH, "//*[@id='menu-content']/div[2]/div/nav/div[5]/div[2]/a[4]").click()
-        # open form newclient
+
+    def add_key(self):
         element = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//*[@id='main-container']/div[1]/div/a[4]")))
         self.driver.find_element(By.XPATH, "//*[@id='main-container']/div[1]/div/a[4]").click()
-        # fill newclient form
-        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-surname']").send_keys("ФамилияАвтоТест")
-        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-name']").send_keys("Имя")
+
+    def fill_newclient_form(self, surname, name, secondname, datapicker, phone, fromwhere):
+        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-surname']").send_keys(surname)
+        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-name']").send_keys(name)
         self.driver.find_element(By.XPATH,
                                  "//*[@id='form_cbase_admin_newClient_input_0_wizard[data][second_name]']").send_keys(
-            "Отчество")
+            secondname)
         self.driver.find_element(By.XPATH, "//*[@id='form_cbase_admin_newClient_date_0__visible']").send_keys(
-            "10102010")
-        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-phone']").send_keys("79278889966")
+            datapicker)
+        self.driver.find_element(By.XPATH, "//*[@id='js-newrec-phone']").send_keys(phone)
         self.driver.find_element(By.XPATH, "//*[@id='select2-chosen-9']").click()
-        self.driver.find_element(By.XPATH, "//*[@id='s2id_autogen9_search']").send_keys("2ГИС")
+        self.driver.find_element(By.XPATH, "//*[@id='s2id_autogen9_search']").send_keys(fromwhere)
         self.driver.find_element(By.XPATH, "//*[@id='s2id_autogen9_search']").send_keys(Keys.ENTER)
-        # submit new patient creation
+
+    def submit_newpatient_creation(self):
         self.driver.find_element(By.XPATH, "//*[@id='js-wizard-form']/div[2]/div[1]/button").click()
         self.driver.find_element(By.XPATH, "//*[@id='js-wizard-form']/div[2]/div[1]/a").click()
 
