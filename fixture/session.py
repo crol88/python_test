70ca0a7371dc5232936e5a6f6e6fde0f985bd655
crol88 <akcrol@gmail.com>
2022-08-24 14:35:40 +0400

    правки

---
+++ fixture/session.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_homepage()
+        wd.find_element(By.ID, "form_user_loginhtml_input_0_login").send_keys(username)
+        wd.find_element(By.ID, "form_user_loginhtml_password_0_password").send_keys(password)
+        wd.find_element(By.ID, "form_user_loginhtml_button_1_button").click()
